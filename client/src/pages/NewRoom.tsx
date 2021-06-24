978641cc06c302e145b9481f424a1fe2078bcf9e
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-06-24 13:22:41 -0300

    Creates NewRoom page

---
+++ client/src/pages/NewRoom.tsx
@@ -0,0 +1,31 @@
+import illustrationImg from '../assets/images/illustration.svg'
+
+import { Button } from '../components/Button'
+
+import '../styles/auth.scss'
+
+export function NewRoom(): JSX.Element {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="chat illustration" />
+        <strong> Create live Q&amp;A rooms </strong>
+        <p> Take your students questions in real-time </p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <h1> Luna App </h1>
+          <h2> Create a new room </h2>
+          <form>
+            <input
+              type="text"
+              placeholder="Type the room code"
+            />
+            <Button type="submit"> Create Room </Button>
+          </form>
+          <p> Do you want to enter in a existing room? <a href="#"> Click Here! </a> </p>
+        </div>
+      </main>
+    </div>
+  )
+}
