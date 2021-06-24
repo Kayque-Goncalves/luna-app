5b7bbd80028565b0979e68d0bbdaeb854b83ab54
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-06-24 15:02:36 -0300

    Adds AuthContext setup to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
+import { AuthContextProvider } from './contexts/AuthContext'
+
 import { Home } from './pages/Home'
 import { NewRoom } from './pages/NewRoom'
 
 function App(): JSX.Element {
   return (
-    <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/rooms/new" component={NewRoom} />
-    </BrowserRouter>
+    <AuthContextProvider>
+      <BrowserRouter>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </BrowserRouter>
+    </AuthContextProvider>
   )
 }
 
