3e1a4df2dd25771403bb867b82af7b508a0744b6
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-06-24 13:22:18 -0300

    Creates Home page

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,35 @@
+import illustrationImg from '../assets/images/illustration.svg'
+import googleIconImg from '../assets/images/google-icon.svg'
+
+import { Button } from '../components/Button'
+
+import '../styles/auth.scss'
+
+export function Home(): JSX.Element {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="chat illustration" />
+        <strong> Create live Q&amp;A rooms </strong>
+        <p> Take your students questions in real-time </p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <h1> Luna App </h1>
+          <button className="create-room">
+            <img src={googleIconImg} alt="Google logo" />
+            Create your room with Google
+          </button>
+          <div className="separator"> or get in an existing room </div>
+          <form>
+            <input
+              type="text"
+              placeholder="Type the room code"
+            />
+            <Button type="submit"> Get started </Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  )
+}
